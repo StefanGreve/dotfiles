331c3c35e656e61e7ec0397b6461c07aa3559b18
StefanGreve <greve.stefan@outlook.jp>
2023-04-15 19:36:46 +0200

    Implement installer script for pwsh

---
+++ scripts/install-pwsh.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+i=1
+n=7
+bin=/usr/local
+arch=$(getconf LONG_BIT)
+default="7.3.4"
+
+pushd $bin > /dev/null
+
+echo "[$i/$n] define pwsh release (e.g. $default)"
+read -p "enter version: " version
+[ -z $version ] && version=$default
+((i++))
+
+echo "[$i/$n] updating system"
+apt-get update
+((i++))
+
+echo "[$i/$n] installing prerequisites"
+apt-get install libssl1.1 libunwind8 -y
+((i++))
+
+echo "[$i/$n] downloading tarball"
+wget https://github.com/PowerShell/PowerShell/releases/download/v$version/powershell-$version-linux-arm$arch.tar.gz
+((i++))
+
+pwshdir=$bin/powershell
+echo "[$i/$n] unpacking tarball to $pwshdir . . ."
+[ -d $pwshdir ] || mkdir $pwshdir
+tar -xvf $bin/powershell-$version-linux-arm$arch.tar.gz -C $pwshdir > /dev/null
+((i++))
+
+echo "[$i/$n] creating symbolic link"
+ln -s $pwshdir/pwsh /usr/bin/pwsh
+((i++))
+
+echo "[$i/$n] installation complete, initiating cleanup . . ."
+rm $bin/powershell-$version-linux-arm$arch.tar.gz --verbose
+popd > /dev/null
+echo "done"
