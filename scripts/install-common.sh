07204e2156c1c248a9822ac2e93e7c8591acbd45
StefanGreve <greve.stefan@outlook.jp>
2023-04-30 12:38:25 +0100

    Update common-install script
    * add git-man for reading Git help pages in the terminal
    * install cargo if it's not already installed on the machine
    * install git-delta, bat, exa crates (note that git-delta/delta
      is required for the global config diff section)
    
    Depending on the hardware, installing Rust crates is a fairly time-
    consuming operation. I'll have to think of a good compromise here
    in the future, so that optional crates may only be installed with
    an opt-in flag. For now though, this is fine for me.

--- scripts/install-common.sh
+++ scripts/install-common.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 i=1
-n=3
+n=5
 
 echo "[$i/$n] updating system"
 apt-get update
@@ -11,10 +11,25 @@ echo "[$i/$n] performing full system upgrade"
 apt-get full-upgrade --yes
 ((i++))
 
-echo "[$i/$n] installing packages"
+echo "[$i/$n] installing apt packages"
 apt-get install \
         neovim \
         neofetch \
+        git-man \
         --yes
+((i++))
+
+echo "[$i/$n] check if cargo is already installed"
+if [ ! -x $(command -v cargo)]; then
+    ./scripts/install-cargo.sh
+fi
+((i++))
+
+echo "[$i/$n] installing rust crates"
+source "$HOME/.cargo/env"
+cargo install \
+      git-delta \
+      bat \
+      exa
 
 echo "done"
